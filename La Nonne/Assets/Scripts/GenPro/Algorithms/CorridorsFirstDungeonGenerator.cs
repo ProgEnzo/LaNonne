aa32b2e8314deb6288cbc7a20e14a883f1e4ed19
enzoprog <e.ferreiradealmeida@rubika-edu.com>
2022-10-06 14:47:49 +0200

    j'ai plus de batterie sur mon clavier

--- La Nonne/Assets/Scripts/GenPro/Algorithms/CorridorsFirstDungeonGenerator.cs
+++ La Nonne/Assets/Scripts/GenPro/Algorithms/CorridorsFirstDungeonGenerator.cs
@@ -2,22 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using Guid = Pathfinding.Util.Guid;
+//using Guid = Pathfinding.Util.Guid;
 
 
 public class CorridorsFirstDungeonGenerator : SimpleRandomWalkMapGenerator
 {
-    [SerializeField] private int corridorLength = 14, corridorCount = 5; //Longueur et nombre de couloirs
-    [SerializeField] [Range(0.1f, 1f)] private float roomPercent = 0.8f; //permet de générer les rooms en fin de couloirs
+    [SerializeField] 
+    private int corridorLength = 14, corridorCount = 5; //Longueur et nombre de couloirs
+    
+    [SerializeField]
+    [Range(0.1f, 1f)] 
+    private float roomPercent = 0.8f; //permet de générer les rooms en fin de couloirs
 
     protected override void RunProceduralGeneration()
     {
-        CorridorsFirstDungeonGeneration();
+        CorridorFirstGeneration();
     }
 
-    private void CorridorsFirstDungeonGeneration()
+    private void CorridorFirstGeneration()
     {
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPositions = new HashSet<Vector2Int>(); 
@@ -35,9 +40,9 @@ public class CorridorsFirstDungeonGenerator : SimpleRandomWalkMapGenerator
     private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPositions)
     {
         HashSet<Vector2Int> roomPositions = new HashSet<Vector2Int>(); //position we return
-        int roomsToCreateToCreateCount = Mathf.RoundToInt(potentialRoomPositions.Count * roomPercent); //nombre de rooms à créer
+        int roomToCreateCount = Mathf.RoundToInt(potentialRoomPositions.Count * roomPercent); //nombre de rooms à créer
 
-        List<Vector2Int> roomsToCreate = potentialRoomPositions.OrderBy(x => Guid.NewGuid()).Take(roomsToCreateToCreateCount).ToList(); //Potential room position in a random order / Randomly sort the HashSet
+        List<Vector2Int> roomsToCreate = potentialRoomPositions.OrderBy(x => Guid.NewGuid()).Take(roomToCreateCount).ToList(); //Potential room position in a random order / Randomly sort the HashSet
 
         foreach (var roomPosition in roomsToCreate)
         {
@@ -61,5 +66,4 @@ public class CorridorsFirstDungeonGenerator : SimpleRandomWalkMapGenerator
             floorPositions.UnionWith(corridor);
         }
     }
-    
 }
