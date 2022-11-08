f6af44da8d0443c96dc5a9e5bd7a31a2fceee66a
enzoprog <e.ferreiradealmeida@rubika-edu.com>
2022-11-08 21:01:46 +0100

    Mise en place des rooms (j'essaie d'implémenter la feature ce soir)

---
+++ La Nonne/Assets/Scripts/GenPro/Rooms/Generator/MapRunTimeGenerator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MapRunTimeGenerator : MonoBehaviour
+{
+    public UnityEvent OnStart;
+    void Start()
+    {
+        OnStart?.Invoke();
+    }
+}
