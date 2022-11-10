05ad40dfc4055eeba4ff4c45dd5b870c207118e2
anaturu <a.marotine@rubika-edu.com>
2022-11-10 16:40:10 +0100

    plein de petits changements + nique cet offset de mort

--- La Nonne/Assets/Scripts/AI/Boss/Attacks/ShrinkingCircleManager.cs
+++ La Nonne/Assets/Scripts/AI/Boss/Attacks/ShrinkingCircleManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class ShrinkingCircleManager : MonoBehaviour
 {
     public GameObject player;
+    public GameObject boss;
 
     public float pushForce;
     public float blackHoleRange;
@@ -16,18 +17,24 @@ public class ShrinkingCircleManager : MonoBehaviour
     private void Awake()
     {
         player = GameObject.FindWithTag("Player");
+        boss = GameObject.FindWithTag("Boss");
     }
 
     private void Update()
     {
         Vector2 direction = (transform.position - player.transform.position).normalized;
-        var distanceBossPlayer = Vector2.Distance(transform.position, player.transform.position);
+        var distanceBossPlayer = Vector2.Distance(boss.transform.position, player.transform.position);
 
         if (distanceBossPlayer < blackHoleRange && PlayerController.instance.m_timerDash < 0)
         {
             player.GetComponent<Rigidbody2D>().AddForce(direction * pushForce, ForceMode2D.Impulse);
 
         }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(boss.transform.position, blackHoleRange);
     }
     
     //DRAW LE BLACK HOLE RANGE ET VOIR COMMENT CA SE FAIT AVEC LE SPRITE RENDERER CIRCLE SHRINKING
