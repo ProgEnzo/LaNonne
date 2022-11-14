166979e2e56abced68a68c614956abfdc395ba0d
Tintin <v.rousselle@rubika-edu.com>
2022-11-14 16:17:34 +0100

    Animator fini, anims pas finies

--- La Nonne/Assets/Scripts/Controller/ChainBlade.cs
+++ La Nonne/Assets/Scripts/Controller/ChainBlade.cs
@@ -83,6 +83,7 @@ namespace Controller
                     isHitting = false;
                     transform.GetChild(0).gameObject.SetActive(false);
                     transform.GetChild(1).gameObject.SetActive(false);
+                    playerAnimator.SetBool(IsAttacking, false);
                 }
             }
         }
@@ -93,8 +94,6 @@ namespace Controller
             yield return new WaitForNextFrameUnit();
             playerAnimator.SetBool(CanChange, false);
             playerAnimator.SetBool(parameterToChange, true);
-            yield return new WaitForNextFrameUnit();
-            playerAnimator.SetBool(parameterToChange, false);
         }
     }
 }
