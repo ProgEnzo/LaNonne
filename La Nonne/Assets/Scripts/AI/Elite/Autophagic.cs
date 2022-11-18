91d1712647997b7fecc6030a6979236f857be0df
Tintin <v.rousselle@rubika-edu.com>
2022-11-18 13:23:14 +0100

    Début autophage

---
+++ La Nonne/Assets/Scripts/AI/Elite/Autophagic.cs
@@ -0,0 +1,42 @@
+using Controller;
+using UnityEngine;
+
+namespace AI.Elite
+{
+    public class Autophagic : EnemyController
+    {
+        private PlayerController playerController;
+        [SerializeField] private float detectionRadius;
+        [SerializeField] private float autoDamagePart;
+        [SerializeField] private float maxTimer;
+        private float currentTimer;
+        private bool isActivated;
+
+        protected override void Update()
+        {
+            base.Update();
+            if (!isActivated)
+            {
+                var distanceToPlayer = Vector2.Distance(playerController.transform.position, transform.position);
+                if (distanceToPlayer < detectionRadius)
+                {
+                    isActivated = true;
+                }
+            }
+            else
+            {
+                if (currentTimer <= 0)
+                {
+                    TakeAutoDamage();
+                    currentTimer = maxTimer;
+                }
+                currentTimer -= Time.deltaTime;
+            }
+        }
+        
+        private void TakeAutoDamage()
+        {
+            currentHealth -= soEnemy.maxHealth * autoDamagePart;
+        }
+    }
+}
