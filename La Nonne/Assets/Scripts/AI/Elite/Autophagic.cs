2c728a7f18cd35567e9492c4d65cba1300ae626a
Tintin <v.rousselle@rubika-edu.com>
2022-11-22 11:06:16 +0100

    Déplacement autophage fonctionnel

--- La Nonne/Assets/Scripts/AI/Elite/Autophagic.cs
+++ La Nonne/Assets/Scripts/AI/Elite/Autophagic.cs
@@ -1,6 +1,8 @@
 using System;
 using Controller;
+using Pathfinding;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace AI.Elite
 {
@@ -11,6 +13,16 @@ namespace AI.Elite
         [SerializeField] private float maxTimer;
         private float currentTimer;
         private bool isActivated;
+        [SerializeField] private float avoidedAngle;
+        [SerializeField] private float destinationMinRangeValue;
+        [SerializeField] private float destinationMaxRangeValue;
+        private int[] positionMultiplierArray = new int[2];
+
+        private void Awake()
+        {
+            positionMultiplierArray[0] = -1;
+            positionMultiplierArray[1] = 1;
+        }
 
         protected override void Update()
         {
@@ -30,6 +42,26 @@ namespace AI.Elite
                 if (currentTimer <= 0)
                 {
                     TakeAutoDamage();
+                    
+                    var newDirection = playerController.transform.position - transform.position;
+                    newDirection.z = 0;
+                    newDirection.Normalize();
+                    var rotationLook = Quaternion.LookRotation(Vector3.forward, newDirection);
+                    bool isNewDirectionOk;
+                    Vector2 destination;
+                    do
+                    {
+                        destination = new Vector2(transform.position.x + Random.Range(destinationMinRangeValue, destinationMaxRangeValue) * positionMultiplierArray[Random.Range(0, 2)], transform.position.y + Random.Range(destinationMinRangeValue, destinationMaxRangeValue) * positionMultiplierArray[Random.Range(0, 2)]);
+                        var newRotationLook = Quaternion.LookRotation(Vector3.forward, destination - (Vector2)transform.position);
+                        isNewDirectionOk = (newRotationLook.eulerAngles.z - rotationLook.eulerAngles.z) switch
+                        {
+                            var angle and >= 0 when angle < avoidedAngle / 2 => false,
+                            var angle and < 0 when angle > -avoidedAngle / 2 => false,
+                            _ => true
+                        };
+                    } while (!isNewDirectionOk);
+                    GetComponent<AIPath>().destination = destination;
+                    
                     currentTimer = maxTimer;
                 }
                 currentTimer -= Time.deltaTime;
