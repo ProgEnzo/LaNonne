39fcbe8fa15b40057db33229bf774f39df39f9f3
Tintin <v.rousselle@rubika-edu.com>
2022-12-02 11:30:19 +0100

    CareTaker et TDI réparés

--- La Nonne/Assets/Scripts/AI/Elite/TDI.cs
+++ La Nonne/Assets/Scripts/AI/Elite/TDI.cs
@@ -10,15 +10,15 @@ namespace AI.Elite
     {
         [Header("Enemy Attack")] 
         [SerializeField] public int bodyDamage;
-        [SerializeField] public int bodyKnockback;
-        [SerializeField] public int circleDamage;
+        [SerializeField] public int bodyKnockBack;
+        [SerializeField] internal int circleDamage;
         [SerializeField] public bool blinkExecuted;
         
         
         [HideInInspector] public float cooldownTimer;
         [SerializeField] private float timeBetweenCircleSpawn;
         
-        [SerializeField] public float healAmount;
+        [SerializeField] internal float healAmount;
         
         [Header("Components")] 
         [SerializeField] public GameObject bully;
@@ -39,22 +39,6 @@ namespace AI.Elite
             CircleTimer();
         }
 
-        private void OnTriggerEnter2D(Collider2D col)
-        {
-            //Heal le TrashMobCLose
-            if (col.gameObject.CompareTag("Enemy"))
-            {
-                col.gameObject.GetComponent<EnemyController>().currentHealth += healAmount;
-                //Debug.Log("<color=orange>TRASH MOB CLOSE</color> HAS BEEN HIT, HEALTH REMAINING : " + col.gameObject.GetComponent<TrashMobClose>().currentHealth);
-            }
-
-            if (col.gameObject.CompareTag("Player"))
-            {
-                col.GetComponent<PlayerController>().TakeDamage(circleDamage); //Player takes damage
-                StartCoroutine(PlayerIsHit());
-            }
-        }
-
         private void OnCollisionEnter2D(Collision2D col) 
         {
             //Si le bully touche le player
@@ -63,11 +47,11 @@ namespace AI.Elite
                 StartCoroutine(PlayerIsHit());
                 playerController.TakeDamage(bodyDamage); //Player takes damage
 
-                Collider2D colCollider = col.collider; //the incoming collider2D (celle du player en l'occurence)
+                var colCollider = col.collider; //the incoming collider2D (celle du player en l'occurence)
                 Vector2 direction = (colCollider.transform.position - transform.position).normalized;
-                Vector2 knockback = direction * bodyKnockback;
+                var knockBack = direction * bodyKnockBack;
             
-                playerController.mRigidbody.AddForce(knockback, ForceMode2D.Impulse);
+                playerController.mRigidbody.AddForce(knockBack, ForceMode2D.Impulse);
             }
         }
 
