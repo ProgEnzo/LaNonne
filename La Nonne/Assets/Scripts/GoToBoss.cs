0a50c56cafddd5adcea5f9225ab7fb420e63521d
ProgEnzo <e.ferreiradealmeida@rubika-edu.com>
2022-12-12 04:27:35 +0100

    MaybeMyLastPushBeforeBeta

--- La Nonne/Assets/Scripts/GoToBoss.cs
+++ La Nonne/Assets/Scripts/GoToBoss.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class GoToBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private RoomContentGenerator roomContentGenerator;
+
+    private void Update()
     {
-        
+        VaLaBas();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void VaLaBas()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //transform.DOMove(new Vector3(roomContentGenerator.mapBoss, roomContentGenerator.mapBoss,0))
+            transform.position = Vector2.MoveTowards(transform.position, roomContentGenerator.mapBoss, 4f);
+        }
     }
 }
