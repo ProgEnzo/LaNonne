b76afd5ed259cd36902be824f4e34156cf9e80cb
Tintin <v.rousselle@rubika-edu.com>
2023-01-13 11:39:19 +0100

    C'est un peu mieux

--- La Nonne/Assets/Scripts/Drag.cs
+++ La Nonne/Assets/Scripts/Drag.cs
@@ -1,20 +1,29 @@
-using System;
 using Shop;
+using Shop.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
     private Canvas canvas;
     private bool isSlotted;
     private int slotIndex;
+    private Vector2 initialAnchoredPosition;
+    private Vector2 initialLocalPosition;
+    private Transform initialParent;
+    [SerializeField] private int buttonNumber;
+    private bool isDragging;
+    internal static bool isAnyDragging;
     
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
         isSlotted = false;
+        initialAnchoredPosition = rectTransform.anchoredPosition;
+        initialLocalPosition = rectTransform.localPosition;
+        initialParent = transform.parent;
     }
 
     private void Update()
@@ -22,25 +31,60 @@ public class Drag : MonoBehaviour, IDragHandler, IEndDragHandler
         if (Input.GetMouseButtonDown(1) && isSlotted)
         {
             EffectManager.instance.appliedEffects[slotIndex] = EffectManager.Effect.None;
-            Destroy(gameObject);
+            rectTransform.anchoredPosition = initialAnchoredPosition;
+            rectTransform.localPosition = initialLocalPosition;
+            transform.SetParent(initialParent);
+            isSlotted = false;
+        }
+
+        if (!isSlotted)
+        {
+            slotIndex = Drop.slotIndex;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (EffectManager.instance.effectInventory[(EffectManager.Effect)buttonNumber] > 0) // && !EffectManager.instance.appliedEffects.Contains((EffectManager.Effect)buttonNumber)
+        {
+            isDragging = true;
+            isAnyDragging = true;
+            WhipModifController.SelectEffect(buttonNumber);
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        if (isDragging)
+        {
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        }
     }
     
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (!Drop.isPointerOnSlot)
+        if (isDragging)
         {
-            Destroy(gameObject);
-        }
-        else
-        {
-            isSlotted = true;
-            slotIndex = Drop.slotIndex;
+            if ((!Drop.isPointerOnSlot || EffectManager.instance.appliedEffects[slotIndex] != EffectManager.Effect.None) && !isSlotted)
+            {
+                rectTransform.anchoredPosition = initialAnchoredPosition;
+            }
+            else if (!Drop.isPointerOnSlot && isSlotted)
+            {
+                EffectManager.instance.appliedEffects[slotIndex] = EffectManager.Effect.None;
+                rectTransform.anchoredPosition = initialAnchoredPosition;
+                rectTransform.localPosition = initialLocalPosition;
+                transform.SetParent(initialParent);
+                isSlotted = false;
+            }
+            else
+            {
+                slotIndex = Drop.slotIndex;
+                isSlotted = true;
+            }
+            
+            isDragging = false;
+            isAnyDragging = false;
         }
     }
 }
