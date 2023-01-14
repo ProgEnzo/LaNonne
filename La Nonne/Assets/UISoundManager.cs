85d9a6efd5657e076732cc224c6dda097d037a25
anaturu <a.marotine@rubika-edu.com>
2023-01-14 22:08:30 +0100

    son d'UI start menu fantasy + lightdrone = goated

---
+++ La Nonne/Assets/UISoundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISoundManager : MonoBehaviour
+{
+    [Header("SoundEffect")]
+    public AudioSource buttonAudioSource;
+    public AudioClip buttonClickAudioClip;
+    public AudioClip buttonHoverAudioClip;
+
+    public void SoundOnClick()
+    {
+        buttonAudioSource.PlayOneShot(buttonClickAudioClip);
+    }
+
+    public void SoundOnHover()
+    {
+        buttonAudioSource.PlayOneShot(buttonHoverAudioClip);
+    }
+}
