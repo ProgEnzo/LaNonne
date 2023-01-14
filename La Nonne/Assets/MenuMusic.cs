8b51de1c866d50bac76b961d030912f2faca40a4
anaturu <a.marotine@rubika-edu.com>
2023-01-14 21:32:29 +0100

    music start screen

---
+++ La Nonne/Assets/MenuMusic.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    [Header("SoundEffect")]
+    public AudioSource musicAudioSource;
+    public AudioClip musicMenuAudioClip;
+
+    private void Start()
+    {
+        musicAudioSource.PlayOneShot(musicMenuAudioClip);
+    }
+}
