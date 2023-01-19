abf41013a748dfa895db3fbeddb9cb0249c39217
enzoprog <e.ferreiradealmeida@rubika-edu.com>
2023-01-19 17:35:55 +0100

    new gen life in rooms & bed's quantity

--- La Nonne/Assets/Scripts/GenPro/Rooms/Generator/RoomContentGenerator.cs
+++ La Nonne/Assets/Scripts/GenPro/Rooms/Generator/RoomContentGenerator.cs
@@ -259,7 +259,7 @@ namespace GenPro.Rooms.Generator
         {
             foreach (var pos in wallUpPos)
             {
-                if (Random.Range(0, 100) < 25)
+                if (Random.Range(0, 100) < 25) //25 = pourcentage d'apparition des props dans la section
                 {
                     Instantiate(wallUpCusto[Random.Range(0, wallUpCusto.Count)], new Vector3(pos.x + 0.5f, pos.y + 0.5f, 0), Quaternion.identity);
                 }
@@ -303,7 +303,7 @@ namespace GenPro.Rooms.Generator
         {
             foreach (var pos in floorPos)
             {
-                if (Random.Range(0, 100) < 80)
+                if (Random.Range(0, 100) < 55)
                 {
                     Instantiate(floorCusto[Random.Range(0, floorCusto.Count)], new Vector3(pos.x + 0.5f, pos.y + 0.5f, 0), Quaternion.identity);
                 }
@@ -314,7 +314,7 @@ namespace GenPro.Rooms.Generator
         {
             foreach (var pos in floorNearWallsUpPos)
             {
-                if (Random.Range(0, 100) < 7)
+                if (Random.Range(0, 100) < 6)
                 {
                     Instantiate(floorNearWallsUpCusto[Random.Range(0, floorNearWallsUpCusto.Count)], new Vector3(pos.x + 0.5f, pos.y, 0), Quaternion.identity);
                 }
@@ -336,7 +336,7 @@ namespace GenPro.Rooms.Generator
         {
             foreach (var pos in floorNearWallsRightPos)
             {
-                if (Random.Range(0, 100) < 25)
+                if (Random.Range(0, 100) < 15)
                 {
                     Instantiate(floorNearWallsRightCusto[Random.Range(0, floorNearWallsRightCusto.Count)], new Vector3(pos.x - 0.5f, pos.y + 0.5f, 0), Quaternion.identity);
                 }
@@ -347,7 +347,7 @@ namespace GenPro.Rooms.Generator
         {
             foreach (var pos in floorNearWallsLeftPos)
             {
-                if (Random.Range(0, 100) < 25)
+                if (Random.Range(0, 100) < 10)
                 {
                     Instantiate(floorNearWallsLeftCusto[Random.Range(0, floorNearWallsLeftCusto.Count)], new Vector3(pos.x + 1.4f, pos.y + 0.3f, 0), Quaternion.identity);
                 }
