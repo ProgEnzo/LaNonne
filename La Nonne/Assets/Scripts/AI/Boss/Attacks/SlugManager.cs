5c1363fd102135643d052bb6f6ff2200649f865a
Tintin <v.rousselle@rubika-edu.com>
2023-01-19 21:27:59 +0100

    Slugs

--- La Nonne/Assets/Scripts/AI/Boss/Attacks/SlugManager.cs
+++ La Nonne/Assets/Scripts/AI/Boss/Attacks/SlugManager.cs
@@ -1,71 +1,94 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using Controller;
 using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class SlugManager : MonoBehaviour
+namespace AI.Boss.Attacks
 {
-    private PlayerController player;
-    public GameObject slugBullet;
-
-    public Rigidbody2D rb;
-
-    public int numberOfBullets;
-    public float bulletSpeed;
-    public int dashAmount;
-    public float massReduction;
-    public float bulletMassReduction;
-    public float dashSpeed;
-
-    private void Start()
+    public class SlugManager : MonoBehaviour
     {
-        player = PlayerController.instance;
-        rb = GetComponent<Rigidbody2D>();
+        private PlayerController player;
+        public GameObject slugBullet;
 
-        GoToNextPoint();
-    }
-    void GoToNextPoint()
-    {
-        StartCoroutine(Go());
-    }
+        public Rigidbody2D rb;
+        private GameObject currentSlugPuppet;
 
-    private IEnumerator Go()
-    {
-        yield return new WaitForSeconds(Random.Range(0f, 3f)); //Randomize
-        
-        dashAmount--;
-        bulletMassReduction += 0.2f;
-        var directionDash = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0) - transform.position.normalized; //Direction Dash
-        rb.velocity = directionDash * dashSpeed; //Dash Slug
-        yield return new WaitForSeconds(Random.Range(0f, 3f)); //Randomize
+        public int numberOfBullets;
+        public float bulletSpeed;
+        public int dashAmount;
+        public float massReduction;
+        public float bulletMassReduction;
+        public float dashSpeed;
 
-        var localScaleProj = slugBullet.transform.localScale;
-
-        for (int i = 0; i < numberOfBullets; i++)
+        private void Start()
         {
-            var slugBulletObject = Instantiate(slugBullet, transform.position, Quaternion.identity); //spawn slugBullet
-            slugBulletObject.transform.localScale += localScaleProj - new Vector3(bulletMassReduction, bulletMassReduction, 0); //reduction masse bullet
+            player = PlayerController.instance;
+            rb = GetComponent<Rigidbody2D>();
             
-            var direction = player.transform.position - transform.position;
-            
-            slugBulletObject.GetComponent<Rigidbody2D>().AddForce(direction * new Vector2(Random.Range(-5f, 10f), Random.Range(-5f, 10f)) * bulletSpeed); //bullet goes to player
-            Destroy(slugBulletObject, 5f);
+            currentSlugPuppet = transform.GetChild(Random.Range(0, transform.childCount)).gameObject;
+            currentSlugPuppet.SetActive(true);
+            GoToNextPoint();
         }
 
-        transform.DOScale(new Vector2(transform.localScale.x - massReduction, transform.localScale.y - massReduction), 0.5f).SetEase(Ease.OutBounce);
-        yield return new WaitForSeconds(1f);
+        private void Update()
+        {
+            CheckDirection();
+        }
 
-        if (dashAmount > 0)
+        private void GoToNextPoint()
         {
             StartCoroutine(Go());
         }
-        else
-        {
-            Destroy(gameObject);
-        }
 
+        private IEnumerator Go()
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 3f)); //Randomize
+        
+            dashAmount--;
+            bulletMassReduction += 0.2f;
+            var directionDash = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0) - transform.position.normalized; //Direction Dash
+            rb.velocity = directionDash * dashSpeed; //Dash Slug
+            yield return new WaitForSeconds(Random.Range(0f, 3f)); //Randomize
+
+            var localScaleProj = slugBullet.transform.localScale;
+
+            for (var i = 0; i < numberOfBullets; i++)
+            {
+                var slugBulletObject = Instantiate(slugBullet, transform.position, Quaternion.identity); //spawn slugBullet
+                slugBulletObject.transform.localScale += localScaleProj - new Vector3(bulletMassReduction, bulletMassReduction, 0); //reduction masse bullet
+            
+                var direction = player.transform.position - transform.position;
+            
+                slugBulletObject.GetComponent<Rigidbody2D>().AddForce(direction * new Vector2(Random.Range(-5f, 10f), Random.Range(-5f, 10f)) * bulletSpeed); //bullet goes to player
+                Destroy(slugBulletObject, 5f);
+            }
+
+            transform.DOScale(new Vector2(transform.localScale.x - massReduction, transform.localScale.y - massReduction), 0.5f).SetEase(Ease.OutBounce);
+            yield return new WaitForSeconds(1f);
+
+            if (dashAmount > 0)
+            {
+                StartCoroutine(Go());
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        private void CheckDirection()
+        {
+            var puppetLocalScale = currentSlugPuppet.transform.localScale;
+            if (rb.velocity.x != 0)
+            {
+                currentSlugPuppet.transform.localScale = new Vector3(MathF.Sign(rb.velocity.x) * MathF.Abs(puppetLocalScale.x), puppetLocalScale.y, puppetLocalScale.z);
+            }
+            else
+            {
+                currentSlugPuppet.transform.localScale = player.transform.position.x > transform.position.x ? new Vector3(MathF.Abs(puppetLocalScale.x), puppetLocalScale.y, puppetLocalScale.z) : new Vector3(-MathF.Abs(puppetLocalScale.x), puppetLocalScale.y, puppetLocalScale.z);
+            }
+        }
     }
 }
